e00adee74b038f726982ca483f2538d27b910e4f
shakila moghadasi <moghadasishakila239@gmail.com>
2022-01-28 17:59:07 +0330

    seri12-1

---
+++ q1/q1.js
@@ -0,0 +1,28 @@
+function validate(){
+const fName = document.getElementById("firstName").value;
+const lName = document.getElementById("lastName").value;
+if(!(fName)||!(lName)||fName.length<3||lName.length<3) {
+    return false;
+}
+else{
+    return true
+}
+}
+// function delay(delaytime) {
+//     myTimeout = setTimeout(div_hide(), delaytime);
+//     return myTimeout
+// }
+function div_show() {
+    if(validate()){
+      document.getElementById('popUp').style.display = "block";
+    //   delay(80000);
+    }
+    else{
+        alert("validation failed");
+    }
+}
+// function div_hide(){
+//     // document.getElementById('popUp').style.display = "none";
+//     document.getElementById('popUp').style.setProperty("display", "none");
+// }
+ 
